0eae0e1137f80bbd054881cc402c879555f936c4
Rushikesh Tade <rushtade@in.ibm.com>
2018-01-28 23:17:33 +0530

    Partial addition of video on off logic

--- repeatStart.js
+++ repeatStart.js
@@ -2,24 +2,86 @@
   if (window.hasRun) {
     return;
   }
+  console.log(browser.getFavicon)
   window.hasRun = true;
-  console.log("Running Code for the video repeater");
-  var myVideo=document.getElementsByTagName("video");
-  var i=0;
-  while(i<myVideo.length){
-    if (typeof myVideo[i].loop == 'boolean') { // loop supported
-      myVideo[i].loop = true;
-      console.log("Enabled loop");
-    } else { // loop property not supported
-      console.log("Enabled Manual loop");
-      myVideo[i].addEventListener('ended', function () {
-        this.currentTime = 0;
-        this.play();
-      }, false);
+
+
+  function SetVideoOnRepeat(){
+      console.log("Running Code for the video repeater");
+      try{
+        var myVideo=document.getElementsByTagName("video");
+        var i=0;
+        while(i<myVideo.length){
+          if (typeof myVideo[i].loop == 'boolean') { // loop supported
+            myVideo[i].loop = true;
+            console.log("Enabled loop");
+          } else { // loop property not supported
+            console.log("Enabled Manual loop");
+            myVideo[i].addEventListener('ended', function () {
+              this.currentTime = 0;
+              this.play();
+                  }, false);
+          }
+        i++;
+        }
+         return true;
     }
-  i++;
+      catch{
+        return false;
+      }
   }
-  return true;
+
+  function SetVideoOffRepeat(){
+    console.log("Running Code for turning off video repeater");
+    var myVideo=document.getElementsByTagName("video");
+    var i=0;
+    while(i<myVideo.length){
+      if (typeof myVideo[i].loop == 'boolean') { // loop supported
+        myVideo[i].loop = false;
+        console.log("Enabled loop");
+      } else { // loop property not supported
+        console.log("Enabled Manual loop");
+        myVideo[i].addEventListener('ended', function () {
+          
+              }, false);
+      }
+    i++;
+    }
+    return true;
+    }
+
+
+function getFavicon(){
+  console.log("Message from the background script:");
+  var favicon = undefined;
+  var nodeList = document.getElementsByTagName("link");
+  for (var i = 0; i < nodeList.length; i++)
+  {
+      if((nodeList[i].getAttribute("rel") == "icon")||(nodeList[i].getAttribute("rel") == "shortcut icon"))
+      {
+          favicon = nodeList[i].getAttribute("href");
+      }
+  }
+  return favicon;
+}
+
+
+  browser.runtime.onMessage.addListener((message) => {
+    if (message.command === "turnon") {
+      var returnValue= SetVideoOnRepeat();
+      return Promise.resolve({response: returnValue});
+    } else if (message.command === "turnoff") {
+      var returnValue=SetVideoOffRepeat();
+      return Promise.resolve({response: returnValue});
+    }
+    else if (message.command === "favicon") {
+      var favicon=  getFavicon();
+      return Promise.resolve({response: favicon});
+    }
+});
+
+
+
 })();
 
 
--- videoRepeater.js
+++ videoRepeater.js
@@ -1,7 +1,8 @@
-
-
+console.log("Video Repeater Started");
+browser.browserAction.onClicked.addListener(onButtonClickedFunction);
 browser.browserAction.setIcon({path: browser.extension.getURL("icons/icon96disabled.png")});
 var LooperTabIds=new Array();
+var contentScriptInjectTabs=new Array();
 
 function notify(message,iconUrl= browser.extension.getURL("icons/icon96.png")) {
   console.log("Displaying message");
@@ -33,6 +34,26 @@ console.log("Video Repeater Started");
 browser.browserAction.onClicked.addListener(onButtonClickedFunction);
 
 browser.tabs.onActivated.addListener(handleActivated);
+browser.tabs.onUpdated.addListener(handleUpdated);
+function handleUpdated(tabId, changeInfo, tabInfo) {
+
+    // if(tabInfo.active){
+      if(contentScriptInjectTabs.includes(tabId)){
+      LooperTabIds = LooperTabIds.filter(item => ![tabId].includes(item));
+      contentScriptInjectTabs=contentScriptInjectTabs.filter(item => ![tabId].includes(item));
+      notify("Due to refresh on " + tabInfo+ " videos will not repeat, click video looper icon again to start repeating ");
+      browser.browserAction.setIcon({path: browser.extension.getURL("icons/icon96disabled.png")}); 
+
+      // console.log("Updated tab: " + tabId);
+      // console.log("Changed attributes: ");
+      // console.log(changeInfo);
+      // console.log("New tab Info: ");
+      // console.log(tabInfo);  
+      }
+    // }
+}
+
+
 function handleActivated(activeInfo) {
   console.log("Tab " + activeInfo.tabId +" was activated");
   if( LooperTabIds.includes(activeInfo.tabId)  ){
@@ -55,22 +76,59 @@ function onButtonClickedFunction(){
   .then(tabs => browser.tabs.get(tabs[0].id))
   .then(tab => {
     console.info(tab);
+    if(contentScriptInjectTabs.includes(tab.id)){
+      if(LooperTabIds.includes(tab.id)){
+        browser.tabs.sendMessage(tab.id, {
+          command: "turnoff",
+      }).then(response => {
+        console.log("Message from the content script:");
+        console.log(response.response);
+      }).catch(onError);
+      LooperTabIds = LooperTabIds.filter(item => ![tab.id].includes(item))
+      console.log(LooperTabIds);
+      notify("Videos on" +tab.title + " will not repeat ");
+      browser.browserAction.setIcon({path: browser.extension.getURL("icons/icon96disabled.png")}); 
+
+    }
+    else{
+      browser.tabs.sendMessage(tab.id, {
+        command: "turnon",
+    }).then(response => {
+      console.log("Message from the content script:");
+      console.log(response.response);
+    }).catch(onError);
+    LooperTabIds[LooperTabIds.length]=tab.id;
+    console.log(LooperTabIds);
+    notify("Videos on" +tab.title + " will auto repeat from now");
+    browser.browserAction.setIcon({path: browser.extension.getURL("icons/icon96.png")}); 
+  
+  }
+
+    }
+    else{
+    contentScriptInjectTabs[contentScriptInjectTabs.length]=tab.id;
+    contentScriptInjectTabs=GetUniqueArray(contentScriptInjectTabs);
+
     var result=browser.tabs.executeScript({
       file: "repeatStart.js"
     });
-    result.then(function(id){
+    result.then(function(id){  
       LooperTabIds[LooperTabIds.length]=tab.id;
       console.log("Added Tab id to looperIds"+tab.id );
       console.log(LooperTabIds);
       LooperTabIds=GetUniqueArray(LooperTabIds)
       console.log(LooperTabIds);
-      notify("Videos on this" +tab.title + " will auto repeat from now");
+      notify("Videos on" +tab.title + " will auto repeat from now");
       browser.browserAction.setIcon({path: browser.extension.getURL("icons/icon96.png")});
 },onFail);
+    }
 
   });
   return;
-  
 }
+
+ 
+  
+// }
   
   
